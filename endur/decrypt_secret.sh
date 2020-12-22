0f9570e7fa5522ff525c90241a6143fe7ef10d42
Harvey Barnhard <harveybarnhard@gmail.com>
2020-12-22 18:47:57 -0500

    Update decrypt_secret.sh

--- endur/decrypt_secret.sh
+++ endur/decrypt_secret.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$STRAVA_TOKENS_PHRASE" \
---output ../data/strava_tokens.json $HOME/data/strava_tokens.json.gpg
+--output ./data/strava_tokens.json ./data/strava_tokens.json.gpg
