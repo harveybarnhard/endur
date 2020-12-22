c703e833f2aca48ba7c91763dfcf20952440194f
Harvey Barnhard <harveybarnhard@gmail.com>
2020-12-22 11:56:14 -0500

    Adding "monday" column back into dataframe

--- data_sample.py
+++ data_sample.py
@@ -33,13 +33,16 @@ activities = activities.pivot_table(
     columns='type',
     values=['distance', 'moving_time', 'total_elevation_gain'],
     fill_value = 0
-)
+).reset_index()
 
 # Collapse multi-index of header columns into one row
-# distance    distance        ->    Hike_distance    Other_distance
-# Hike        Other
+
+# distance  |  distance        ->    Hike_distance  |  Other_distance
+# Hike      |  Other
+
 new_cols = [('{1}_{0}'.format(*tup)) for tup in activities.columns]
 activities.columns = new_cols
+activities = activities.rename({'_monday': 'monday'}, axis='columns')
 
 # Write data
 activities.to_csv('data/strava_activities_sub.csv', index=False)
