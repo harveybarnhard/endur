da1758dc30ea6a48a67853492c2c38f65462381d
Harvey Barnhard <harveybarnhard@gmail.com>
2020-12-30 15:23:38 -0500

    Update create_badge.sh

--- .github/workflows/create_badge.sh
+++ .github/workflows/create_badge.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 datenow=$(date "+%s")
-sed -i "s|date\/\(.*\)|date\/$datenow\?color=FC4C02\&label=Last%20Updated|" ../../README.md
+sed -i "s|date\/\(.*\)|date\/$datenow\?color=FC4C02\&label=Last%20Updated\)|" ../../README.md
