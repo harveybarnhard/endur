e3436a4b89dd8dca596f8eeaf6cfd42f3287396d
Harvey Barnhard <40671793+harveybarnhard@users.noreply.github.com>
2023-04-06 20:40:17 -0400

    Update data_sample.py

--- endur/data_sample.py
+++ endur/data_sample.py
@@ -27,7 +27,7 @@ df = pd.DataFrame({
 })
 df['monday'] = df['monday'].dt.strftime('%m-%d-%Y')
 activities['monday'] = activities['monday'].dt.strftime('%m-%d-%Y')
-activities = pd.concat(df).reset_index()
+activities = pd.reset_index()
 
 # Convert active hundredths of seconds to active hours
 activities['moving_time'] = activities['moving_time']/3600
